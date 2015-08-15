7a4b270a9895ee251536e7f67450ee162a0917ce
Kornel Labun <kornellabun@gmail.com>
2015-08-15 19:57:30 +0200

    added one test for directive, two-way bindings

--- public/modules/projects/tests/projects.client.directive.test.js
+++ public/modules/projects/tests/projects.client.directive.test.js
@@ -3,19 +3,28 @@
 (function() {
     describe('Projects Directive Tests', function() {
         var scope,
-            $compile;
+            $compile,
+            element;
 
         beforeEach(module(ApplicationConfiguration.applicationModuleName));
         beforeEach(inject(function($rootScope, _$compile_) {
             scope = $rootScope.$new();
             $compile = _$compile_;
+
+            scope.parse = 'www.linkme.com';
+            element = $compile('<external-links parse="parse"></external-links>')(scope);
+            scope.$digest();
         }));
 
         it('Replaces the element with the appropriate content', function() {
-            scope.parse = 'www.linkme.com';
-            var element = $compile('<external-links parse="parse"></external-links>')(scope);
-            scope.$digest();
             expect(element.html()).toContain('<a href="http://www.linkme.com" target="_blank">linkme.com</a>');
+        });
+
+        it('parse on isolated scope should be two-way bound', function() {
+            var isolatedScope = element.isolateScope();
+            isolatedScope.parse = 'something new';
+            scope.$digest();
+            expect(scope.parse).toEqual('something new');
         });
     });
 }());
